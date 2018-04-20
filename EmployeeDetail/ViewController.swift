a9683a974f963c01b06c84727389d6ee2b6f1248
Arihant Patil <arya@arihants-macbook-air.local>
2018-04-20 10:29:15 +0530

    Initial Commit

---
+++ EmployeeDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmployeeDetail
+//
+//  Created by Arihant Patil on 20/04/18.
+//  Copyright © 2018 Payal Wankhede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
